87e5e7109b6b815121b9a32fc09255bcf60e38c3
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Air.local>
2021-08-23 17:09:54 -0700

    works

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const LZString = require("lz-string");
+
+const app = express();
+
+const encode = (str = "") => LZString.compressToEncodedURIComponent(str)
+const decode = (str = "") => LZString.decompressFromEncodedURIComponent(str)
+
+const data = JSON.stringify({
+	test : "hey hey hey",
+	data : "hooya",
+})
+
+app.get("/", (req, res) => {
+	console.log("data", data)
+	res.redirect(`http://localhost:5000/decode?note=${encode(data)}`);
+})
+
+
+app.get("/decode", (req, res) => {
+	const {
+		note : encoded = ""
+	} = req.query;
+
+	console.log("note", encoded)
+	console.log("decoded", decode(encoded));
+	res.send(decode(encoded));
+})
+
+app.listen(5000, () => {
+	console.log("we runnin up in here")
+	console.log("catch us at http://localhost:5000")
+})
